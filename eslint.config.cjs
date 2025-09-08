e1e6cb2c3c7bc1bbbeaf19a9dacf58a83ece5cd5
Mads Bjerre <mads@madsb.com>
2025-09-08 15:53:09 +0200

    Rename eslint.config.js -> eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,21 +1,21 @@
-const js = require('@eslint/js');
-const typescript = require('@typescript-eslint/eslint-plugin');
-const typescriptParser = require('@typescript-eslint/parser');
-const vue = require('eslint-plugin-vue');
-const vueParser = require('vue-eslint-parser');
-const importPlugin = require('eslint-plugin-import');
-const prettier = require('eslint-config-prettier');
-const globals = require('globals');
+const js = require('@eslint/js')
+const typescript = require('@typescript-eslint/eslint-plugin')
+const typescriptParser = require('@typescript-eslint/parser')
+const vue = require('eslint-plugin-vue')
+const vueParser = require('vue-eslint-parser')
+const importPlugin = require('eslint-plugin-import')
+const prettier = require('eslint-config-prettier')
+const globals = require('globals')
 
 module.exports = [
   // Base JavaScript recommended rules
   js.configs.recommended,
-  
+
   // Global ignores
   {
-    ignores: ['**/node_modules/**', '**/*.d.ts', '**/*.js', '**/dist/**']
+    ignores: ['**/node_modules/**', '**/*.d.ts', '**/*.js', '**/dist/**'],
   },
-  
+
   // Vue files configuration
   {
     files: ['**/*.vue'],
@@ -24,16 +24,16 @@ module.exports = [
       parserOptions: {
         parser: typescriptParser,
         ecmaVersion: 'latest',
-        sourceType: 'module'
-      }
+        sourceType: 'module',
+      },
     },
     plugins: {
       vue,
-      '@typescript-eslint': typescript
+      '@typescript-eslint': typescript,
     },
     rules: {
       ...vue.configs['vue3-recommended'].rules,
-      
+
       // Vue specific rules
       'vue/component-definition-name-casing': ['error', 'kebab-case'],
       'vue/html-indent': [
@@ -44,53 +44,53 @@ module.exports = [
           baseIndent: 1,
           closeBracket: 0,
           alignAttributesVertically: false,
-          ignores: []
-        }
+          ignores: [],
+        },
       ],
       'vue/singleline-html-element-content-newline': [
         'error',
         {
           ignoreWhenNoAttributes: true,
           ignoreWhenEmpty: true,
-          ignores: ['pre', 'textarea']
-        }
+          ignores: ['pre', 'textarea'],
+        },
       ],
       'vue/block-tag-newline': ['error', { singleline: 'always', multiline: 'always' }],
       'vue/first-attribute-linebreak': [
         'error',
         {
           singleline: 'ignore',
-          multiline: 'below'
-        }
+          multiline: 'below',
+        },
       ],
       'vue/max-attributes-per-line': [
         'error',
         {
           singleline: {
-            max: 1
+            max: 1,
           },
           multiline: {
-            max: 1
-          }
-        }
+            max: 1,
+          },
+        },
       ],
       'vue/multi-word-component-names': 'off',
-      
+
       // Accessibility rules turned off (from vuejs-accessibility)
       // Note: vuejs-accessibility plugin not updated for ESLint 9 yet
-      
+
       // Unused vars rule
       'no-unused-vars': [
         'warn',
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_'
-        }
-      ]
-    }
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
-  
+
   // TypeScript files configuration
   {
     files: ['**/*.ts', '**/*.tsx'],
@@ -98,16 +98,16 @@ module.exports = [
       parser: typescriptParser,
       parserOptions: {
         ecmaVersion: 'latest',
-        sourceType: 'module'
-      }
+        sourceType: 'module',
+      },
     },
     plugins: {
       '@typescript-eslint': typescript,
-      import: importPlugin
+      import: importPlugin,
     },
     rules: {
       ...typescript.configs.recommended.rules,
-      
+
       // TypeScript specific rules
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-non-null-assertion': 'off',
@@ -116,8 +116,8 @@ module.exports = [
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_'
-        }
+          caughtErrorsIgnorePattern: '^_',
+        },
       ],
       '@typescript-eslint/no-shadow': ['error'],
       'no-shadow': 'off',
@@ -126,32 +126,32 @@ module.exports = [
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_'
-        }
-      ]
-    }
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
-  
+
   // General configuration for all files
   {
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
-        
+
         // Vue 3 macros
         defineProps: 'readonly',
         defineEmits: 'readonly',
         defineExpose: 'readonly',
-        withDefaults: 'readonly'
-      }
-    }
+        withDefaults: 'readonly',
+      },
+    },
   },
-  
+
   // Prettier configuration (disables conflicting rules)
   {
     rules: {
-      ...prettier.rules
-    }
-  }
-];
+      ...prettier.rules,
+    },
+  },
+]
